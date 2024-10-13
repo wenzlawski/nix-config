799ce17fc8266ac826d783012a011a7a8cefb733
Marc Wenzlawski <marc.wenzlawski@gmail.com>
2024-10-13 20:11:50 +0200

    move pkgs to local-pkgs

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,14 +6,14 @@
 }: {
   # This one brings our custom packages from the 'pkgs' directory
   # borg = self.callPackage ./borg { };
-  # additions = final: _prev: {
-  #   # nest everything under a namespace that's not likely to collide
-  #   # with anything in nixpkgs
-  #   local-pkgs = import ../pkgs {pkgs = final;};
-  # };
+  additions = final: _prev: {
+    # nest everything under a namespace that's not likely to collide
+    # with anything in nixpkgs
+    local-pkgs = import ../pkgs {pkgs = final;};
+  };
 
   # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs final.pkgs;
+  # additions = final: _prev: import ../pkgs final.pkgs;
 
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
