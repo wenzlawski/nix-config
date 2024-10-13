b9c9a84c192b52298ce67f5006924014fb0c0391
Marc Wenzlawski <marc.wenzlawski@gmail.com>
2024-10-13 20:09:36 +0200

    remove emacs package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,8 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
-# {pkgs ? (import ../nixpkgs.nix) {}}: {
-pkgs: {
+{pkgs ? (import ../nixpkgs.nix) {}}: {
+  # pkgs: {
   borg-scripts = pkgs.callPackage ./borg {};
   xpo = pkgs.callPackage ./xpo {};
   testhello = pkgs.callPackage ./testhello.nix {};
-  # emacs-30 = pkgs.callPackage ./emacs-30.nix {};
-  # emacs-30withpkgs = (pkgs.emacsPackagesFor pkgs.emacs-30).emacsWithPackages (epkgs: [
-  #   epkgs.jinx
-  #   epkgs.vterm
-  # ]);
 }
