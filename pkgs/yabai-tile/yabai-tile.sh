6a95edb2abf749f51f584bcd5f454e7bb758f75d
Marc Wenzlawski <marc.wenzlawski@gmail.com>
2024-11-10 02:53:15 +0100

    Fix window tiling calculations by adjusting display height and check conditions

--- pkgs/yabai-tile/yabai-tile.sh
+++ pkgs/yabai-tile/yabai-tile.sh
@@ -11,10 +11,10 @@ focused_display=$(yabai -m query --displays | jq 'map(select(."has-focus" == tru
 # Extract the display's frame properties
 display_frame=$(echo "$focused_display" | jq '.frame')
 display_width=$(echo "$display_frame" | jq '.w' | awk '{print int($1)}')
-display_height=$(echo "$display_frame" | jq '.h' | awk '{print int($1)}')
+display_height=$(echo "$display_frame" | jq '.h' | awk '{print int($1) - 25}')
 half_display_width=$(echo "$display_width / 2" | bc)
 third_display_width=$(echo "$display_width / 3" | bc)
-half_display_height=$(echo "$display_height / 2 - 12" | bc)
+half_display_height=$(echo "$display_height / 2" | bc)
 
 # Extract the window frame properties
 window_width=$(echo "$window_frame" | jq '.w' | awk '{print int($1)}')
@@ -24,7 +24,7 @@ window_y=$(echo "$window_frame" | jq '.y' | awk '{print int($1) - 25}')
 
 if [ "$DIRECTION" == left ]; then
 	echo "Tiling left"
-	CHECK_1_0=$((window_width == half_display_width - 1))
+	CHECK_1_0=$((window_width == half_display_width - 1 && window_height == display_height))
 	CHECK_1_1=$((window_x == 0))
 	TILE_1="1:3:0:0:2:1" # left 2/3
 	TILE_0="1:2:0:0:1:1" # left 1/2
@@ -35,7 +35,7 @@ if [ "$DIRECTION" == left ]; then
 
 elif [ "$DIRECTION" == right ]; then
 	echo "tiling right"
-	CHECK_1_0=$((window_width == half_display_width - 1))
+	CHECK_1_0=$((window_width == half_display_width - 1 && window_height == display_height))
 	CHECK_1_1=$((window_x == half_display_width + 1))
 	TILE_1="1:3:1:0:2:1" # right 2/3
 	TILE_0="1:2:1:0:1:1" # right 1/2
@@ -45,23 +45,23 @@ elif [ "$DIRECTION" == right ]; then
 	TILE_2="1:3:2:0:1:1" # right 1/3
 elif [ "$DIRECTION" == up ]; then
 	echo "tiling up"
-	CHECK_1_0=$((window_height == half_display_height - 1 && window_width == display_width))
+	CHECK_1_0=$((window_height == half_display_height && window_width == display_width))
 	CHECK_1_1=$((window_y == 0))
 	TILE_1="2:2:0:0:1:1" # top left 1/4
 	TILE_0="2:1:0:0:1:1" # top 1/2
 
-	CHECK_2_0=$((window_height == half_display_height - 1 && window_width == half_display_width - 1))
+	CHECK_2_0=$((window_height == half_display_height && window_width == half_display_width - 1))
 	CHECK_2_1=$((CHECK_1_1 && window_x != half_display_width + 1))
 	TILE_2="2:2:1:0:1:1" # top right 1/4
 
 elif [ "$DIRECTION" == down ]; then
 	echo "tiling down"
-	CHECK_1_0=$((window_height == half_display_height - 1 && window_width == display_width))
-	CHECK_1_1=$((window_y == half_display_height + 1))
+	CHECK_1_0=$((window_height == half_display_height && window_width == display_width))
+	CHECK_1_1=$((window_y == half_display_height + 2))
 	TILE_1="2:2:0:1:1:1" # bottom left 1/4
 	TILE_0="2:1:0:1:1:1" # bottom 1/2
 
-	CHECK_2_0=$((window_height == half_display_height - 1 && window_width == half_display_width - 1))
+	CHECK_2_0=$((window_height == half_display_height && window_width == half_display_width - 1))
 	CHECK_2_1=$((CHECK_1_1 && window_x != half_display_width + 1))
 	TILE_2="2:2:1:1:1:1" # bottom right 1/4
 else
