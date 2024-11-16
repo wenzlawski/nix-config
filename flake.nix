4910583c812186057fa9fa1ccf963b288e08b591
Marc Wenzlawski <marc.wenzlawski@gmail.com>
2024-11-16 22:56:11 +0100

    Update flake.lock and flake.nix to track new commits and switch nixpkgs branch to nixpkgs-24.05-darwin

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   inputs = {
     # Where we get most of our software. Giant mono repo with recipes
     # called derivations that say how to build software.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Manages configs links things into your home directory
